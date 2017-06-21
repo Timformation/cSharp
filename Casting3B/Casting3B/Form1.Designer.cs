5b6f1a3e8afd379c69894e169c809af1580d0190
Timformation <Timformation@users.noreply.github.com>
2017-06-20 17:26:29 -0700

    CIS 146 Intro
    Address program and casting assignment 3B.

---
+++ Casting3B/Casting3B/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Casting3B
+{
+   
+    partial class Form1
+    {
+       
+
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNum4
+            // 
+            this.txtNum4.Location = new System.Drawing.Point(109, 80);
+            this.txtNum4.Name = "txtNum4";
+            this.txtNum4.Size = new System.Drawing.Size(100, 20);
+            this.txtNum4.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtNum4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum4;
+    }
+}
+
---
+++ Casting3B/Casting3B/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Casting3B
+{
+    public partial class Form1 : Form
+    {
+        int intNum1 = 30;
+        double dblNum2 = 45.3;
+        string strNum3;
+        int intNum5;
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNum4.Text = intNum1.ToString();
+            strNum3 = txtNum4.Text;
+            dblNum2 = intNum1;
+            intNum1 = int.Parse(dblNum2);
+            txtNum4.Text = (dblNum2 * 2).ToString();
+            intNum1 = int.Parse(txtNum4.Text);
+            dblNum2 = double.Parse(txtNum4.Text);
+            txtNum4.Text = (dblNum2 * 2).ToString();
+            txtNum4.Text = ("The value is " + dblNum2.ToString("c")).ToString();
+
+
+
+
+        }
+    }
+
+
+
